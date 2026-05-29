ea9f9890163f3482fbf6fe8f7db1f34067f4793c
koustavdeb-git <koustavdeb2015@gmail.com>
2026-05-29 14:09:53 +0530

    calender ui fixes

--- src/components/employees/AddEmployeeModal.jsx
+++ src/components/employees/AddEmployeeModal.jsx
@@ -199,6 +199,12 @@ const AddEmployeeModal = ({ isOpen, onClose, onEmployeeAdded }) => {
                                     calendarClassName="custom-calendar"
                                     className="mt-1 w-[100%] px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                     dateFormat="yyyy-MM-dd"
+                                    popperModifiers={[
+                                        {
+                                            name: "flip",
+                                            enabled: false,
+                                        },
+                                    ]}
                                 />
                             </div>
                             <div>
